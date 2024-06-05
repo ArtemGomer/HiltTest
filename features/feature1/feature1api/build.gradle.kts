2f2ede861a14679877b4b57f1372ec6ed4e85939
a.gomer <Fhntv7878>
2024-06-05 17:38:52 +0700

    Очистка проекта

---
+++ features/feature1/feature1api/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+dependencies {
+    implementation(project(":features:feature2:feature2api"))
+}
+
